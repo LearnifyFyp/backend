28c83245efc574fe1231da925dd06afe78a360e3
letslearnwebapp <letslearn647@gmail.com>
2023-05-17 10:54:19 +0500

    learnify backend

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ import { Server } from 'socket.io';
 const io = new Server(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'https://lets-learn-project.netlify.app',
+        origin: 'https://learnify-project.netlify.app/',
     },
 });
 
