afd6ed9ce9fc6e36c251ac22ff751668f4928748
Hussain-Tanveer <officialhussain3@gmail.com>
2023-10-15 08:58:33 +0500

    chnaes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ import catchAsyncErrors from "./catchAsyncErrors.js";
 import jwt from "jsonwebtoken";
 import User from "../models/users.js";
 
-
 // export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 //     const { token } = req.cookies;
 
@@ -18,58 +17,55 @@ import User from "../models/users.js";
 //     next();
 // });
 
-
 export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-    let token;
+  let token;
 
-    for (let i = 0; i < req.rawHeaders.length; i++) {
-        const check = req.rawHeaders[i].split(" ");
-        if (check[0] == "Bearer") {
-            token = check;
-        }
+  for (let i = 0; i < req.rawHeaders.length; i++) {
+    const check = req.rawHeaders[i].split(" ");
+    if (check[0] == "Bearer") {
+      token = check;
     }
+  }
 
-    if (!token[1]) {
-        return next(new ErrorHandler("Please Login to access this resource", 401));
-    }
+  if (!token[1]) {
+    return next(new ErrorHandler("Please Login to access this resource", 401));
+  }
 
-    const decodedData = jwt.verify(token[1], process.env.JWT_SECRET);
+  const decodedData = jwt.verify(token[1], process.env.JWT_SECRET);
 
-    req.user = await User.findById(decodedData.id);
+  req.user = await User.findById(decodedData.id);
 
-    next();
+  next();
 });
-
 
 // Roles For Admin And User //
 export const authorizeRolesForAdmin = (...rolesForAdmin) => {
-    return (req, res, next) => {
-        if (!rolesForAdmin.includes(req.user.roleForAdmin)) {
-            return next(
-                new ErrorHandler(
-                    `Role: ${req.user.roleForAdmin} is not allowed to access this resource `,
-                    403
-                )
-            );
-        }
+  return (req, res, next) => {
+    if (!rolesForAdmin.includes(req.user.roleForAdmin)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.roleForAdmin} is not allowed to access this resource `,
+          403
+        )
+      );
+    }
 
-        next();
-    };
+    next();
+  };
 };
-
 
 // Roles For Tutor And Student //
 export const authorizeRoles = (...roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            return next(
-                new ErrorHandler(
-                    `Role: ${req.user.role} is not allowed to access this resource `,
-                    403
-                )
-            );
-        }
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resource `,
+          403
+        )
+      );
+    }
 
-        next();
-    };
+    next();
+  };
 };
--- server.js
+++ server.js
@@ -5,86 +5,79 @@ import cloudinary from "cloudinary";
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Uncaught Exception`);
-    process.exit(1);
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
 });
-
 
 // config env file //
 config({
-    path: "./config/config.env",
-})
-
+  path: "./config/config.env",
+});
 
 // connecting to database //
 connectDatabase();
 
-
 // cloudinary connection //
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
-
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
 // Create socket.io server
 const io = new Server(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: 'http://localhost:3000',
-    },
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
-
 
 io.on("connection", (socket) => {
-    console.log("Connected to socket.io");
-    socket.on("setup", (userData) => {
-        socket.join(userData._id);
-        socket.emit("connected");
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    console.log(newMessageRecieved, "newMessageRecievednewMessageRecieved");
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
+  });
 
-    socket.on("join chat", (room) => {
-        socket.join(room);
-        console.log("User Joined Room: " + room);
-    });
-    socket.on("typing", (room) => socket.in(room).emit("typing"));
-    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-    socket.on("new message", (newMessageRecieved) => {
-        var chat = newMessageRecieved.chat;
-
-        if (!chat.users) return console.log("chat.users not defined");
-
-        chat.users.forEach((user) => {
-            if (user._id == newMessageRecieved.sender._id) return;
-
-            socket.in(user._id).emit("message recieved", newMessageRecieved);
-        });
-    });
-
-    socket.off("setup", () => {
-        console.log("USER DISCONNECTED");
-        socket.leave(userData._id);
-    });
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
-
-
 
 // Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-    server.close(() => {
-        process.exit(1);
-    });
+  server.close(() => {
+    process.exit(1);
+  });
 });
